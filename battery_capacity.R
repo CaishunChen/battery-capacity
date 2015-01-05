969aec558dfec5fc5239b0b3b0a0b73c30762771
Nathan Campos <eeepc904@gmail.com>
2015-01-05 16:29:46 -0200

    Moved everything to R

--- battery_capacity.R
+++ battery_capacity.R
@@ -4,47 +4,83 @@
 #' @author Nathan Campos <nathanpc@dreamintech.net>
 
 library("ggplot2")
-setwd("~/Developer/Statistics/data/Battery-Capacity/")
+library("scales")
+setwd("~/Developer/Statistics/data/Battery-Capacity/data")
+rm(list = ls(all = TRUE))  # Clear the workspace.
 
-
-###
-###
-### FUCK THIS SHIT. ggplot2 is fucking horrible.
-###
-###
-
-
-parse_csv <- function (csvfile, current) {
+# Get the battery discharge data from the CSV file.
+battery_discharge <- function (name, csvfile, current, cutoff) {
   csv = read.csv(csvfile, header = FALSE)
-  volts = unlist(csv[3])
+  volts = csv[[3]]
   mah = c()
 
   for (i in 0:(length(volts) - 1)) {
     mah = c(mah, current * (i / 3600))
     
-    if (i > 0 && volts[i] < 0.8) {
+    if (i > 0 && volts[i] < cutoff) {
       length(volts) <- i + 1
       break;
     }
   }
   
-  return(data.frame(voltage = volts, mah = mah))
+  return(data.frame(voltage = volts, mah = mah, name = name))
 }
 
-batteries = c(parse_csv("MOX-AA-3600mAh-Discharge.log", 200))
-
 # Plotting.
-#plot_mah <- function (vlist) {
+plot_mah <- function (batts) {
   graph = ggplot()
+  graph = graph + scale_colour_brewer(palette="Set1")
+  graph = graph + theme(legend.title = element_blank(),
+                        legend.justification = c(1, 1),
+                        legend.position = c(1, 1))
 
   for (i in 1:length(batteries)) {
-    graph = graph + geom_line(aes(x = mah, y = voltage), batteries[i])
+    graph = graph + geom_line(data = batts[[i]],
+                              aes(x = mah, y = voltage, color = name))
   }
 
   # Setup labels and etc.
-  graph = graph + scale_x_continuous("Capacity (mAh)")
-  graph = graph + scale_y_continuous("Voltage (V)")
-  graph = graph + ggtitle("AA Battery Discharge at 200mA")
+  graph = graph + scale_x_continuous("Capacity (mAh)",
+                                     breaks = pretty_breaks(n = 10))
+  graph = graph + scale_y_continuous("Voltage (V)",
+                                     breaks = pretty_breaks(n = 10))
+  #graph = graph + ggtitle("AA Battery Discharge at 200mA")
   
-  graph
-#}
+  print(graph)
+}
+
+# Get batteries by type.
+get_batteries <- function (type) {
+  csv = read.csv(paste(type, "index.csv", sep = "/"))
+  batts = list()
+  
+  for (i in 1:nrow(csv)) {
+    battery = csv[i,]
+    
+    if (battery$show == 1) {
+      model = battery$model
+      capacity = battery$exp_capacity
+      
+      if (model != "") {
+        model = paste0(" ", model)
+      }
+      
+      if (!is.na(capacity)) {
+        capacity = sprintf(" %smAh", capacity)
+      } else {
+        capacity = ""
+      }
+      
+      name = sprintf("%s%s %sV%s", battery$brand, model, battery$voltage, capacity)
+      batts[[length(batts) + 1]] = battery_discharge(name,
+                                                     paste(type, battery$file, sep = "/"),
+                                                     battery$current,
+                                                     battery$cutoff)
+    }
+  }
+  
+  return(batts)
+}
+
+batteries <- get_batteries("AA")
+plot_mah(batteries)
